42f72d70c0f4bc1ab08314b5897b8cec7367c059
martinamca <martinamca@gmail.com>
2014-04-30 23:55:59 +0300

    Models and Databases bölümünü bitirdim.
    5. bölümün kondlarını ve yaptığım değişiklikleri sunucuya gönderiyorum.

---
+++ tango/rango/admin.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8-*-
+from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
