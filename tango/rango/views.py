1bbcfe80f93567c6e1e024408d0c5215d73c93e7
martinamca <martinamca@gmail.com>
2014-04-30 21:24:16 +0300

    templates ve static media bölümünü bitirdim
    4. bölüm olan templates ve static media bölümünü bitirdikten sonra
    yaptığım değişiklikleri commit ediyorum.

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python
 #-*-coding:utf-8-*-
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse('Rango projesi size merhaba diyor!')
+    context = RequestContext(request)
+    context_dict = {'boldmessage': 'Bağlamdan gelen ve kalın gözükecek mesaj burada'}
+    return render_to_response('rango/index.html', context_dict, context)
+
+def about(request):
+    context = RequestContext(request)
+    context_dict = {'boldmessage': 'Bağlamdan gelen ve kalın gözükecek mesaj burada'}
+    return render_to_response('rango/about.html', context_dict, context)
